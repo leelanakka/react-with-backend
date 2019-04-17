e3995efb78c301d3610989bb7978802105fd87cd
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-17 15:50:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { domainToUnicode } from "url";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  transfer() {
+    const name = document.getElementById("name").value;
+    console.log(name);
+    fetch("/transfer", { method: "POST", body: name })
+      .then(res => res.text())
+      .then(text => {
+        console.log(text);
+        alert(text);
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form>
+        Name:
+        <input id="name" type="text" name="name" />
+        <input
+          type="submit"
+          value="Submit"
+          onClick={this.transfer.bind(this)}
+        />
+      </form>
     );
   }
 }
