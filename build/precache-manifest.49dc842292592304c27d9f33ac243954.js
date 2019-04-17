a348128b88cfc2b3bef9c91017dd6543697caeba
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-17 17:21:33 +0530

    added build folder

---
+++ build/precache-manifest.49dc842292592304c27d9f33ac243954.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cc1b4b4ac9774cd677b",
+    "url": "/static/js/main.56c20a48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d9d26f5553689b7e797",
+    "url": "/static/js/2.902c9faf.chunk.js"
+  },
+  {
+    "revision": "7e14abbcffa17ddbb352618b3292ef98",
+    "url": "/index.html"
+  }
+];
